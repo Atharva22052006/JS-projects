8485f3100e3be30aa685ea1369b9199c3af948d3
Atharva22052006 <101049431+Atharva22052006@users.noreply.github.com>
2024-12-16 21:07:15 +0530

    Add files via upload

---
+++ Projects js/Project 1/project1.js
@@ -0,0 +1,16 @@
+// deposite some money
+// Determine the amount user wants to send
+// Amount to bet on 
+// Spin the slot machine
+// Check if user won 
+// give the user their warning
+// play again
+
+const prompt= require("prompt-sync")();
+
+const deposite=()=>{
+    const depositeAmount= prompt("Enter Deposite Amount: ")
+};
+
+deposite();
+
